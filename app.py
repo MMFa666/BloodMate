4543279e7b62fc5b5c0de0e0236b386d44aaee5b
Muhammad Mubashar <mubashar1030@hotmail.com>
2020-12-13 04:40:25 +0500

    'Completed'

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 import numpy as np
 from datetime import datetime
 from datetime import timedelta 
